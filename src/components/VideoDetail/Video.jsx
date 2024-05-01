00428a0ad40ceecd44df78df298eb4963c96921f
sanjana <03sanjana2004@gmail.com>
2024-05-02 00:39:04 +0530

    next

--- src/components/VideoDetail/Video.jsx
+++ src/components/VideoDetail/Video.jsx
@@ -17,9 +17,23 @@ const Video = () => {
             </div>
             <h3>
 Create A Website Like YouTube Using HTML CSS JS | YouTube Clone Website Design Step By Step</h3>
-<div className="play-video-info">
+
 
     <p>1225 Views  &bull; 2days ago</p>
+    <div className="play-video-info" >
+        <div className="channel-info">
+
+        <img src="https://img.freepik.com/free-vector/cinema-realistic-poster-with-illuminated-bucket-popcorn-drink-3d-glasses-reel-tickets-blue-background-with-tapes-vector-illustration_1284-77070.jpg?size=626&ext=jpg&ga=GA1.1.553209589.1714521600&semt=sph" alt="" />
+        <h1>Great Code</h1>
+        </div>
+        <div className="options">
+        <img src="https://www.freeiconspng.com/thumbs/youtube-like-png/youtube-like-button-png-11.png" alt="" />
+        <img src="https://www.shutterstock.com/image-vector/thumbs-down-unlike-button-on-260nw-1471034171.jpg"  alt="" />
+        <img src="https://png.pngtree.com/png-clipart/20191121/original/pngtree-share-icon-isolated-on-background-png-image_5093179.jpg" alt="" />
+        </div>
+
+        
+   
 </div>
 
             </div>
