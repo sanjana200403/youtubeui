be11aeb7550f4ff11d03eed050775d07dff0f99e
sanjana <03sanjana2004@gmail.com>
2024-05-02 10:20:26 +0530

    ready

--- src/components/VideoDetail/Video.jsx
+++ src/components/VideoDetail/Video.jsx
@@ -20,23 +20,38 @@ Create A Website Like YouTube Using HTML CSS JS | YouTube Clone Website Design S
 
 
     <p>1225 Views  &bull; 2days ago</p>
+    {/* <hr /> */}
     <div className="play-video-info" >
         <div className="channel-info">
 
         <img src="https://img.freepik.com/free-vector/cinema-realistic-poster-with-illuminated-bucket-popcorn-drink-3d-glasses-reel-tickets-blue-background-with-tapes-vector-illustration_1284-77070.jpg?size=626&ext=jpg&ga=GA1.1.553209589.1714521600&semt=sph" alt="" />
         <h1>Great Code</h1>
+        <button>Subscribe</button>
         </div>
         <div className="options">
         <img src="https://www.freeiconspng.com/thumbs/youtube-like-png/youtube-like-button-png-11.png" alt="" />
         <img src="https://www.shutterstock.com/image-vector/thumbs-down-unlike-button-on-260nw-1471034171.jpg"  alt="" />
         <img src="https://png.pngtree.com/png-clipart/20191121/original/pngtree-share-icon-isolated-on-background-png-image_5093179.jpg" alt="" />
         </div>
+       
 
         
    
 </div>
 
+<div className="play-desc">
+    <h4>
+547K views  2 years ago</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, voluptatibus. Asperiores iste molestiae magni labore eius accusamus officia facere architecto provident aut nesciunt officiis ad, quas vero, suscipit eos perspiciatis quasi dolore voluptatum eum.</p>
+        </div>
+        <hr/>
+        <h4>Add Comment</h4>
+        
+      
+
+
             </div>
+           
             <div className="right-sidebar">
 <div className="side-video-list">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTag9zJ7DkwtS_B0IqOJloZQjp-XTM6FwDnbGzEZbqeeg&s" className='small-img' alt="" />
